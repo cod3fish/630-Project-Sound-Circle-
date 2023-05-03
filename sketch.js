35eb45bc6d20e3dfb6b2291d60b80f1e3a008124
cod3fish <jayasankar1029@gmail.com>
2023-05-03 00:51:11 -0400

    debug2

--- sketch.js
+++ sketch.js
@@ -1,50 +1,3 @@
-// Replace with your own API key
-// const apiKey = 'YOUR_API_KEY';
-
-// let planets = [];
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-
-//   // Create planets
-//   for (let i = 0; i < 10; i++) {
-//     planets.push({
-//       x: random(width),
-//       y: random(height),
-//       size: random(20, 100),
-//       color: color(random(255), random(255), random(255))
-//     });
-//   }
-
-  // Add event listener to the submit button
-//   const submitButton = document.getElementById('submit-color');
-//   submitButton.addEventListener('click', () => {
-//     const colorInput = document.getElementById('color-input').value;
-//     updateColorScheme(colorInput);
-//   });
-// }
-
-// function draw() {
-//   background(0);
-
-//   // Draw planets
-//   for (let planet of planets) {
-//     fill(planet.color);
-//     ellipse(planet.x, planet.y, planet.size);
-//   }
-// }
-
-// async function updateColorScheme(baseColor) {
-//   const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&count=10&mode=analogic&apiKey=${apiKey}`);
-//   const data = await response.json();
-
-  // Update planet colors
-//   for (let i = 0; i < planets.length; i++) {
-//     planets[i].color = color(data.colors[i].rgb.r, data.colors[i].rgb.g, data.colors[i].rgb.b);
-//   }
-// }
-
-
 let colors = [];
 async function generateColorScheme() {
     const userInput = document.getElementById("color-input").value.slice(1);
@@ -90,14 +43,14 @@ function setup() {
     stars.push(new Star());
   }
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 9; i++) {
     planets.push(new Planet());
   }
   
 }
 
 function draw() {
-//   background(0);
+  background(0);
 
   for (let star of stars) {
     star.show();
@@ -106,6 +59,17 @@ function draw() {
   for (let planet of planets) {
     planet.show();
   }
+
+  beginShape();
+  let waveLength = 100;
+  let amplitude = 50;
+  for (let x = 0; x < width; x += 5) {
+    let y = height / 2 + amplitude * sin((x + frameCount) * TWO_PI / waveLength);
+    vertex(x, y);
+  }
+  endShape();
+
+  
 }
 
 class Star {
